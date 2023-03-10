c6cc9ce369a1faac34ed0e057cc5780f13723ee7
DenverLacey <36467790+DenverLacey@users.noreply.github.com>
2023-03-10 22:07:27 +1100

    Consolidate Optional implementations.

--- sk/optional.h
+++ sk/optional.h
@@ -13,33 +13,59 @@ namespace sk {
                 return s << "None";
             }
         };
+
+        template<typename T>
+        class OptionalInner {
+        public:
+            OptionalInner() noexcept : _has_value(false) {}
+            OptionalInner(TNone) noexcept : _has_value(false) {}
+            OptionalInner(T value) noexcept : _has_value(true), _value(std::move(value)) {}
+
+        public:
+            bool is_some() const noexcept { return this->_has_value; }
+            bool is_none() const noexcept { return !this->_has_value; }
+
+        protected:
+            T& get() noexcept { assert(_has_value); return this->_value; }
+            const T& get() const noexcept { assert(_has_value); return this->_value; }
+            void set(const T& value) noexcept { _has_value = true; _value = value; }
+
+        private:
+            bool _has_value;
+            T _value;
+        };
+
+        template<typename T>
+        class OptionalInner<T&> {
+        public:
+            OptionalInner() noexcept : _value(nullptr) {}
+            OptionalInner(TNone) noexcept : _value(nullptr) {}
+            OptionalInner(T& value) noexcept : _value(&value) {}
+
+        public:
+            bool is_some() const noexcept { return this->_value != nullptr; }
+            bool is_none() const noexcept { return this->_value == nullptr; }
+
+        protected:
+            T& get() noexcept { assert(_value != nullptr); return *this->_value; }
+            const T& get() const noexcept { assert(_value != nullptr); return *this->_value; }
+            void set(const T& value) noexcept { *_value = value; }
+
+        private:
+            T* _value;
+        };
     }
 
     inline constexpr internal::TNone None = {};
 
     template<typename T>
-    struct Optional {
-        // === Data ===
-        bool _has_value;
-        T _value;
+    class Optional : public internal::OptionalInner<T> {
+    public:
+        Optional() noexcept : internal::OptionalInner<T>() {}
+        Optional(T value) noexcept : internal::OptionalInner<T>(value) {}
+        Optional(internal::TNone) noexcept : internal::OptionalInner<T>(None) {}
 
-        // === Constructors / Assignments ===
-        Optional() noexcept :
-            _has_value(false)
-        {
-        }
-
-        Optional(T value) noexcept :
-            _has_value(true),
-            _value(std::move(value))
-        {
-        }
-
-        Optional(internal::TNone) noexcept :
-            _has_value(false)
-        {
-        }
-
+    public:
         Optional<T>& operator=(const Optional<T>&) noexcept = default;
         Optional<T>& operator=(Optional<T>&&) noexcept = default;
 
@@ -49,40 +75,31 @@ namespace sk {
         }
 
         Optional<T>& operator=(const T& value) noexcept {
-            this->_has_value = true;
-            this->_value = value;
+            this->set(value);
             return *this;
         }
 
         // === Associated Functions ===
-        bool is_some() const noexcept {
-            return this->_has_value;
-        }
-
-        bool is_none() const noexcept {
-            return !this->_has_value;
-        }
-
         T& unwrap() noexcept {
-            internal::ensure(this->_has_value, "Unwrap of None value attempted.");
-            return this->_value;
+            internal::ensure(this->is_some(), "Unwrap of None value attempted.");
+            return this->get();
         }
 
         T& expect(const char* message) noexcept {
-            internal::ensure(this->_has_value, message);
-            return this->_value;
+            internal::ensure(this->is_some(), message);
+            return this->get();
         }
 
         T unwrap_or(const T& default_value) noexcept {
-            if (this->_has_value) {
-                return this->_value;
+            if (this->is_some()) {
+                return this->get();
             }
             return default_value;
         }
 
         bool try_unwrap(T& out_value) const noexcept {
-            if (this->_has_value) {
-                out_value = this->_value;
+            if (this->is_some()) {
+                out_value = this->get();
                 return true;
             }
             return false;
@@ -90,18 +107,18 @@ namespace sk {
 
         template<typename F>
         Optional<std::invoke_result_t<F, T>> map(F&& f) noexcept {
-            if (!this->_has_value) {
+            if (this->is_none()) {
                 return None;
             }
-            return f(this->_value);
+            return f(this->get());
         }
 
         template<typename F>
         std::invoke_result_t<F, T> map_or(const std::invoke_result_t<F, T>& default_value, F&& f) noexcept {
-            if (!this->_has_value) {
+            if (this->is_none()) {
                 return default_value;
             }
-            return f(this->_value);
+            return f(this->get());
         }
 
         // === Friends ===
@@ -113,99 +130,6 @@ namespace sk {
                 s << "None";
             }
 
-            return s;
-        }
-    };
-
-    template<typename T>
-    struct Optional<T&> {
-        // === Data ===
-        T* _value;
-
-
-        // === Constructors / Assignments ===
-        Optional(T& value) noexcept :
-            _value(&value)
-        {
-        }
-
-        Optional(internal::TNone) noexcept :
-            _value(nullptr)
-        {
-        }
-
-        Optional<T&>& operator=(const Optional<T&>&) noexcept = default;
-        Optional<T&>& operator=(Optional<T&>&&) noexcept = default;
-
-        Optional<T&>& operator=(internal::TNone) noexcept {
-            this->_value = nullptr;
-            return *this;
-        }
-
-        Optional<T&>& operator=(const T& value) noexcept {
-            *this->_value = value;
-            return *this;
-        }
-
-        // === Associated Functions ===
-        bool is_some() const noexcept {
-            return this->_value != nullptr;
-        }
-
-        bool is_none() const noexcept {
-            return this->_value == nullptr;
-        }
-
-        T& unwrap() noexcept {
-            internal::ensure(this->is_some(), "Unwrap of None value attempted.");
-            return *this->_value;
-        }
-
-        T& expect(const char* message) noexcept {
-            internal::ensure(this->is_some(), message);
-            return *this->_value;
-        }
-
-        T unwrap_or(const T& default_value) noexcept {
-            if (this->is_some()) {
-                return *this->_value;
-            }
-            return default_value;
-        }
-
-        bool try_unwrap(T& out_value) const noexcept {
-            if (this->is_some()) {
-                out_value = *this->_value;
-                return true;
-            }
-            return false;
-        }
-
-        template<typename F>
-        Optional<std::invoke_result_t<F, T>> map(F&& f) noexcept {
-            if (this->is_none()) {
-                return None;
-            }
-            return f(*this->_value);
-        }
-
-        template<typename F>
-        std::invoke_result_t<F, T> map_or(const std::invoke_result_t<F, T>& default_value, F&& f) noexcept {
-            if (this->is_none()) {
-                return default_value;
-            }
-            return f(*this->_value);
-        }
-
-        // === Friends ===
-        friend std::ostream& operator<<(std::ostream& s, const Optional<T&>& opt) noexcept {
-            T val;
-            if (opt.try_unwrap(val)) {
-                s << "Some(" << val << ")";
-            } else {
-                s << "None";
-            }
-            
             return s;
         }
     };
